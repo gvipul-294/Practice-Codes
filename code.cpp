ec3031ee76d995829d65e50dfc3fa5a0172416f8
Vipul Gupta <gvipul294@gmail.com>
2018-05-19 23:59:24 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -5,10 +5,10 @@ int main() {
 	// your code goes here
 	long i,j;
 	long temp_health, health=2000, temp_inj=1, max_inj=1000000; //max_inj stores maximum injury possible while temp_inj will contain solution for particular i 
-	long ans=0;  // Basically ans will contain the final solution 
+	long ans=0,temp_ans=0;  // Basically ans will contain the final solution 
 	long n;
 	cin>>n;                // n denotes the number of employees
-	int arr[n];
+	long* arr=new long[n];
 	for(i=0; i<n; i++){
 	    cin>>arr[i];
 	}
@@ -16,13 +16,15 @@ int main() {
 	    j=i;
 	    temp_inj=1;
 	    temp_health=health;
-	    while(j<n&&(temp_inj*arr[j])<max_inj&&(temp_health-arr[j])>0){
+	    temp_ans=0;
+	    while(j<n&&((temp_inj*arr[j])<max_inj)&&((temp_health-arr[j])>0)){
 	     temp_inj*=arr[j];
 	     temp_health-=arr[j];
-	     j++;   
+	     j++;  
+	     temp_ans++;
 	    }
-	    if(ans<j){
-	        ans=j;
+	    if(ans<temp_ans){
+	        ans=temp_ans;
 	    }
 	}
 	cout<<ans<<endl;
