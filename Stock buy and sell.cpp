b2da30bf4f5da9ef2104887f63dbe3fdae9ba9d0
Vipul Gupta <gvipul294@gmail.com>
2018-06-13 19:06:55 +0530

    Create Stock buy and sell.cpp

---
+++ Stock buy and sell.cpp
@@ -0,0 +1,16 @@
+/*
+
+PROBLEM
+The cost of a stock on each day is given in an array, find the max profit that you can make by buying and selling in those days. 
+
+*/
+
+//REVIEW : See https://www.geeksforgeeks.org/stock-buy-sell/
+
+Basic Algo:
+
+find local minima and local maxima
+
+
+
+
